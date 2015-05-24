8fc5f6825854af6c8de76e93f50ac6b3de3b4684
lycanf <fangking1988@gmail.com>
2015-05-24 12:10:15 +0800

    add
    add

---
+++ tsd-launcher/src/com/tuyou/tsd/launcher/HomeActivity.java
@@ -0,0 +1,245 @@
+package com.tuyou.tsd.launcher;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.ProgressDialog;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.tuyou.tsd.R;
+import com.tuyou.tsd.common.CommonMessage;
+import com.tuyou.tsd.common.TSDEvent;
+import com.tuyou.tsd.common.base.BaseActivity;
+import com.tuyou.tsd.common.util.LogUtil;
+import com.tuyou.tsd.core.CoreService;
+import com.tuyou.tsd.core.CoreService.ContentType;
+import com.tuyou.tsd.core.CoreService.WorkingMode;
+
+public class HomeActivity extends BaseActivity {
+	private static final String TAG = "HomeActivity";
+
+	private static ProgressDialog mLoadingDialog;
+	private IntentFilter mIntentFilter;
+
+	private ImageView mFaceView;
+	private ImageButton mMusicBtn, mNewsBtn, mNavBtn, mPodBtn;
+	private TextView mCallBtn, mAllAppsBtn;
+
+	private CoreService mCoreService;
+
+	// For cell phone debug only
+//	private boolean mAccState; // true -- on; false -- off.
+//	private int mClickedTimes;
+
+	private ServiceConnection mServiceConnection = new ServiceConnection() {
+		
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			Log.v(TAG, "onServiceDisconnected.");
+			mCoreService = null;
+		}
+		
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			Log.v(TAG, "onServiceConnected.");
+			mCoreService = ((CoreService.LocalBinder)service).getService();
+			if (mCoreService.isLoadingCompleted() && mLoadingDialog != null) {
+				LogUtil.v(TAG, "dismiss the loading diaLogUtil.");
+				mLoadingDialog.dismiss();
+				mLoadingDialog = null;
+			} else {
+				LogUtil.w(TAG, "ignore dismiss dialog, mCoreService.isLoadingCompleted()=" + mCoreService.isLoadingCompleted()
+						+ ", mLoadingDialog=" + mLoadingDialog);
+			}
+		}
+	};
+
+	private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			LogUtil.d(TAG, "receive the broadcast: " + intent);
+			String action = intent.getAction();
+
+			if (action.equals(CommonMessage.VOICE_COMM_WAKEUP)) {
+				onWakeUp();
+			}
+
+			if (action.equals(TSDEvent.System.LOADING_COMPLETE)) {
+				if (mLoadingDialog != null) {
+					LogUtil.v(TAG, "dismiss the loading diaLogUtil.");
+					mLoadingDialog.dismiss();
+					mLoadingDialog = null;
+				} else {
+					LogUtil.w(TAG, "LoadingDialog is null, skip the event.");
+				}
+			}
+		}
+		
+	};
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		Log.v(TAG, "onCreate");
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home_activity_2);
+
+		mIntentFilter = new IntentFilter();
+		mIntentFilter.addAction(TSDEvent.System.LOADING_COMPLETE);
+		mIntentFilter.addAction(CommonMessage.VOICE_COMM_WAKEUP);
+
+		initView();
+		initService();
+		showLoadingDialog();
+	}
+
+	@Override
+	protected void onResume() {
+		Log.v(TAG, "onResume");
+		super.onResume();
+		setIconClick(false);
+		registerReceiver(mReceiver, mIntentFilter);
+		bindService(new Intent(this, CoreService.class), mServiceConnection, Service.BIND_AUTO_CREATE);
+	}
+
+	@Override
+	protected void onPause() {
+		Log.v(TAG, "onPause");
+		super.onPause();
+
+		unregisterReceiver(mReceiver);
+		unbindService(mServiceConnection);
+	}
+
+	@Override
+	protected void onDestroy() {
+		Log.v(TAG, "onDestroy");
+		super.onDestroy();
+	}
+
+//	@Override
+//	protected void onSaveInstanceState(Bundle outState) {
+//		outState.putParcelable("weather", mWeatherData);
+//		super.onSaveInstanceState(outState);
+//	}
+
+	private void initView() {
+		mFaceView = (ImageView) findViewById(R.id.home_xiaobao_face);
+		mMusicBtn = (ImageButton) findViewById(R.id.home_music_btn);
+		mNewsBtn  = (ImageButton) findViewById(R.id.home_news_btn);
+		mNavBtn   = (ImageButton) findViewById(R.id.home_nav_btn);
+		mPodBtn   = (ImageButton) findViewById(R.id.home_pod_btn);
+		mCallBtn  = (TextView) findViewById(R.id.home_icall_btn);
+		mAllAppsBtn = (TextView) findViewById(R.id.home_allapps_btn);
+
+		mMusicBtn.setOnClickListener(mClickListener);
+		mNewsBtn.setOnClickListener(mClickListener);
+		mNavBtn.setOnClickListener(mClickListener);
+		mPodBtn.setOnClickListener(mClickListener);
+		mCallBtn.setOnClickListener(mClickListener);
+		mAllAppsBtn.setOnClickListener(mClickListener);
+	}
+	
+
+	private void initService() {
+		startService(new Intent(this, CoreService.class));
+	}
+
+	private void onWakeUp() {
+		mFaceView.setImageResource(R.drawable.xiaobao_wakeup);
+	}
+
+	private void onPrepare() {
+		mFaceView.setImageResource(R.drawable.xiaobao_nor);
+	}
+
+	private void showLoadingDialog() {
+//		LogUtil.v(TAG, "showLoadingDialog()");
+		if (mLoadingDialog == null) {
+			LogUtil.v(TAG, "create the loading diaLogUtil.");
+			LoadingDialog df = LoadingDialog.newInstance();
+			df.show(getFragmentManager(), "loadingDialog");
+		} else {
+			LogUtil.w(TAG, "Loading dialog is already shown, ignore.");
+		}
+	}
+
+	public static class LoadingDialog extends DialogFragment {
+
+		static LoadingDialog newInstance() {
+			return new LoadingDialog();
+		}
+
+		@Override
+		public Dialog onCreateDialog(Bundle savedInstanceState) {
+			mLoadingDialog = new ProgressDialog(getActivity());
+			mLoadingDialog.setMessage("系统正在启动中...");
+			mLoadingDialog.setIndeterminate(true);
+//			mLoadingDiaLogUtil.setCancelable(true);
+//			LogUtil.v(TAG, "LoadingDiaLogUtil.onCreateDialog, mLoadingDialog = " + mLoadingDialog);
+            return mLoadingDialog;
+		}
+		
+	}
+
+	private MyOnClickListener mClickListener = new MyOnClickListener();
+	private volatile boolean mbIsClickedIcon = false;
+	private void setIconClick(boolean bClick){
+		LogUtil.w(TAG, "setIconClick mbIsClickedIcon="+mbIsClickedIcon);
+		mbIsClickedIcon = bClick;
+	}
+	private boolean isIconClicked(){
+		LogUtil.w(TAG, "isIconClicked mbIsClickedIcon="+mbIsClickedIcon);
+		return mbIsClickedIcon;
+	}
+	private class MyOnClickListener implements View.OnClickListener {
+		@Override
+		public void onClick(View v) {
+			LogUtil.w(TAG, "MyOnClickListener mbIsClickedIcon="+mbIsClickedIcon);
+			if(!isIconClicked()){
+				setIconClick(true);
+			}
+			switch (v.getId()) {
+			case R.id.home_music_btn:
+				if (mCoreService != null) {
+					mCoreService.changeMode(WorkingMode.MODE_AUDIO, ContentType.TYPE_MUSIC);
+				}
+				break;
+			case R.id.home_news_btn:
+				if (mCoreService != null) {
+					mCoreService.changeMode(WorkingMode.MODE_AUDIO, ContentType.TYPE_NEWS);
+				}
+				break;
+			case R.id.home_nav_btn:
+				if (mCoreService != null) {
+					mCoreService.changeMode(WorkingMode.MODE_MAP, ContentType.TYPE_MAP);
+				}
+				break;
+			case R.id.home_pod_btn:
+				if (mCoreService != null) {
+					mCoreService.changeMode(WorkingMode.MODE_AUDIO, ContentType.TYPE_JOKE);
+				}
+				break;
+			case R.id.home_icall_btn:
+				// TODO:
+				break;
+			case R.id.home_allapps_btn:
+				startActivity(new Intent(HomeActivity.this, AppsActivity.class));
+				break;
+			}
+		}
+	}
+
+}
---
+++ tsd-voiceassistant/src/com/tuyou/tsd/voice/InteractingActivity.java
@@ -0,0 +1,304 @@
+package com.tuyou.tsd.voice;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.KeyEvent;
+
+import com.tuyou.tsd.common.CommonMessage;
+import com.tuyou.tsd.common.TSDEvent;
+import com.tuyou.tsd.common.util.HelperUtil;
+import com.tuyou.tsd.common.util.LogUtil;
+import com.tuyou.tsd.voice.service.VoiceAssistant;
+
+public class InteractingActivity extends Activity {
+	private static final String TAG = "InteractingActivity";
+
+	private Fragment mRecordFragment, mRecogFragment, mSearchFragment, mErrorFragment;
+//	private ListView mResultListView;
+//	private DestinationAdapter mDestAdapter;
+
+//	private boolean mIsRecording = false;
+
+	//
+	// 用于与VoiceAssistant service
+	//
+	private static Messenger mVoiceService = null;
+	private final Messenger mMessenger = new Messenger(new VoiceEngineMsgHandler());
+
+	enum FRAGMENT_TYPE {
+		RECORD,
+		RECOG,
+		SEARCH,
+		ERROR
+	}
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		Log.d(TAG, "onCreate...");
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.interacting_activity);
+
+		initView();
+
+		bindService(new Intent(this, VoiceAssistant.class), mVoiceServiceConnection, Service.BIND_AUTO_CREATE);
+
+		IntentFilter filter = new IntentFilter();
+		filter.addAction(TSDEvent.Interaction.FINISH_ACTIVITY);
+		filter.addAction(CommonMessage.VOICE_COMM_WAKEUP);
+		registerReceiver(mReceiver, filter);
+	}
+
+	@Override
+	protected void onResume() {
+		Log.d(TAG, "onResume...");
+		if (mVoiceService != null) {
+			try {
+				// Register self for reply message
+				Message msg = Message.obtain(null, CommonMessage.VoiceEngine.REGISTER_CLIENT);
+				msg.replyTo = mMessenger;
+				mVoiceService.send(msg);
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
+		}
+
+		super.onResume();
+	}
+
+	@Override
+	protected void onPause() {
+		Log.d(TAG, "onPause...");
+		if (mVoiceService != null) {
+			try {
+				// Register self for reply message
+				Message msg = Message.obtain(null, CommonMessage.VoiceEngine.UNREGISTER_CLIENT);
+				msg.replyTo = mMessenger;
+				mVoiceService.send(msg);
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
+		}
+
+		super.onPause();
+	}
+
+	@Override
+	protected void onDestroy() {
+		Log.d(TAG, "onDestroy...");
+		unbindService(mVoiceServiceConnection);
+		unregisterReceiver(mReceiver);
+		super.onDestroy();
+	}
+
+	private void initView() {
+		mRecordFragment = new RecordFragment();
+		mRecogFragment = new RecognitionFragment();
+		mSearchFragment = new SearchFragment();
+		mErrorFragment = new ErrorFragment();
+
+		FragmentManager fm = getFragmentManager();
+		FragmentTransaction trans = fm.beginTransaction();
+		trans.add(R.id.fragment_container, mRecordFragment);
+		trans.commit();
+	}
+
+	private void playBing() {
+		// Play hint sound
+		MediaPlayer player = MediaPlayer.create(this, R.raw.altair);
+		player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+				mp.release();
+				mp = null;
+			}
+		});
+		player.start();
+	}
+
+	private FRAGMENT_TYPE mFragmentType = FRAGMENT_TYPE.RECORD;
+	private void transFragment(FRAGMENT_TYPE type) {
+		Log.v(TAG,"transFragment FRAGMENT_TYPE "+type);
+		Fragment fragment = null;
+		mFragmentType = type;
+		switch(type){
+		case RECORD:
+			fragment = mRecordFragment;
+			break;
+		case RECOG:
+			fragment = mRecogFragment;
+			break;
+		case SEARCH:
+			fragment = mSearchFragment;
+			break;
+		case ERROR:
+			fragment = mErrorFragment;
+			break;
+		}
+		FragmentTransaction transaction = getFragmentManager().beginTransaction();
+		transaction.replace(R.id.fragment_container, fragment);
+		transaction.commit();
+	}
+
+	/**
+	 * 语音助手service binding状态回调
+	 */
+	private ServiceConnection mVoiceServiceConnection = new ServiceConnection() {
+		
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			mVoiceService = null;
+//			Toast.makeText(getApplicationContext(), "VoiceAssistant service disconnected.", Toast.LENGTH_SHORT).show();
+		}
+		
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			mVoiceService = new Messenger(service);
+	
+			try {
+				// Register self for reply message
+				Message msg = Message.obtain(null, CommonMessage.VoiceEngine.REGISTER_CLIENT);
+				msg.replyTo = mMessenger;
+				mVoiceService.send(msg);
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
+	
+//			Toast.makeText(getApplicationContext(), "VoiceAssistant service connected.", Toast.LENGTH_SHORT).show();			
+		}
+	};
+
+	void sendMessageToService(int what, Bundle data) {
+		if (mVoiceService != null) {
+			try {
+				Message msg = Message.obtain(null, what);
+				msg.replyTo = mMessenger;
+				if (data != null) {
+					msg.setData(data);
+				}
+				mVoiceService.send(msg);
+			} catch (RemoteException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * 用于同VoiceAssistant service通信
+	 */
+	private class VoiceEngineMsgHandler extends Handler {
+
+		@Override
+		public void handleMessage(Message msg) {
+
+			switch(msg.what) {
+//			case CommonMessage.VoiceEngine.TTS_PLAY_BEGIN:
+//				mFaceButton.setBackgroundResource(R.drawable.intact_face_speaking);
+//				break;
+
+//			case CommonMessage.VoiceEngine.INTERACTION_START:
+//				mCloseButton.setVisibility(View.GONE);
+//				break;
+
+//			case CommonMessage.VoiceEngine.INTERACTION_STOP:
+//				mCloseButton.setVisibility(View.VISIBLE);
+//				break;
+
+			case CommonMessage.VoiceEngine.RECORDING_START:
+				((RecordFragment)mRecordFragment).startAnimation();
+				break;
+
+			case CommonMessage.VoiceEngine.RECORDING_STOP:
+				((RecordFragment)mRecordFragment).stopAnimation();
+				break;
+
+			case CommonMessage.VoiceEngine.RECORDING_VOLUME:
+				break;
+
+			case CommonMessage.VoiceEngine.RECOGNITION_START:
+				transFragment(FRAGMENT_TYPE.RECOG);
+				break;
+
+			case CommonMessage.VoiceEngine.RECOGNITION_COMPLETE:
+				((RecognitionFragment)mRecogFragment).setResultText(msg.getData().getString("result"));
+				playBing();
+				break;
+
+			case CommonMessage.VoiceEngine.RECOGNITION_CANCEL:
+			case CommonMessage.VoiceEngine.RECOGNITION_ERROR:
+				String error = msg.getData().getString("result");
+				if ( !error.equals("用户取消") ) {
+					transFragment(FRAGMENT_TYPE.ERROR);
+					((ErrorFragment)mErrorFragment).setErrorText(error);
+					playBing();
+				}
+				break;
+
+			case CommonMessage.VoiceEngine.SEARCH_BEGIN:
+				((RecognitionFragment)mRecogFragment).setStatusText(getResources().getString(R.string.searching));
+				break;
+
+			case CommonMessage.VoiceEngine.SEARCH_END:
+				transFragment(FRAGMENT_TYPE.SEARCH);
+				((SearchFragment)mSearchFragment).setResultData(msg.getData().getString("result"));
+				break;
+
+			default:
+				super.handleMessage(msg);
+			}
+		}
+
+	}
+	
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// TODO Auto-generated method stub
+		Log.v(TAG,"keycode = "+keyCode);
+		switch (keyCode) {
+		case KeyEvent.KEYCODE_F4:
+			Log.v(TAG,"onKeyDown mFragmentType="+mFragmentType);
+			Log.v(TAG,"onKeyDown VoiceAssistant getState="+VoiceAssistant.getState());
+			if(mFragmentType!=FRAGMENT_TYPE.SEARCH){
+				Log.v(TAG,"onKeyDown not in search view");
+				break;
+			}
+
+			break;
+		default:
+				
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
+	private BroadcastReceiver mReceiver = new BroadcastReceiver() {
+		
+		@SuppressWarnings("unchecked")
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			String action = intent.getAction();
+			LogUtil.d(TAG, "BroadcastReceiver.onReceive, action: " + action);
+
+			if (action.equals(TSDEvent.Interaction.FINISH_ACTIVITY)) {
+				HelperUtil.finishActivity(InteractingActivity.this, android.R.anim.fade_in, android.R.anim.fade_out);
+			}else if(action.equals(CommonMessage.VOICE_COMM_WAKEUP)){
+				
+			}
+		}
+	};
+}
